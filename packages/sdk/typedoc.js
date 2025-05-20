809d0ed517eb87a77e564f16ee21ef884584636c
Sergey Chystiakov <anyxem@gmail.com>
2025-05-20 09:58:24 +0200

    feat: add docs generator

---
+++ packages/sdk/typedoc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entryPoints: ['src/mintlayer-connect-sdk.ts'],
+  plugin: ['typedoc-plugin-markdown'],
+  out: 'docs',
+  hideBreadcrumbs: true,
+  hidePageTitle: true,
+  includeVersion: false,
+};
