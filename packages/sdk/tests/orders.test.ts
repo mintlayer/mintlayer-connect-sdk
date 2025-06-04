2f30b0da0e3911ed13e50462f9160c58a6b469f2
Sergey Chystiakov <anyxem@gmail.com>
2025-06-04 23:31:50 +0200

    test: fix

--- packages/sdk/tests/orders.test.ts
+++ packages/sdk/tests/orders.test.ts
@@ -237,7 +237,7 @@ test('fill order', async () => {
       "atoms": "40400000000",
       "decimal": "0.404",
     },
-    "id": "ee22e819eba9b30b3b3ecd21322b566d41973d346a922eb9a685121554ad3be1",
+    "id": "7cf8eb89b786869160e47e27b25cbf577dc2112516cf5b147cd22bd341a18d66",
     "inputs": [
       {
         "input": {
